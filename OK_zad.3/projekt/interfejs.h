16c67ab67daaad3404faee9c645b8f0fc3c13ea2
mac229 <mac229@interia.eu>
2013-11-19 16:08:07 +0100

    INTERFEJS.H
    opisy funkcji

--- OK_zad.3/projekt/interfejs.h
+++ OK_zad.3/projekt/interfejs.h
@@ -6,12 +6,46 @@ using namespace std;
 
 /***********
 *		TODO List		
-*	opisz funkcje!!!!!
+*	
 ***********/
 
-
+/***********
+*	@ czas_wykonania
+*		funkcja pokazuj¹ca mo¿liwe opcje wyboru zakresu losowanych liczb dla czasów wykonania procesów
+*	@ Argumenty:
+*		
+*	@ Zwraca:
+*		wybran¹ opjê			
+* 
+***********/
 int czas_wykonania();
+
+
+
+/***********
+*	@ liczba_przestojow
+*		funkcja pokazuj¹ca mo¿liwe opcje wyboru liczby losowanych przestojow
+*	@ Argumenty:
+*		
+*	@ Zwraca:
+*		wybran¹ opjê	
+* 
+***********/
 int liczba_przestojow();
+
+
+/***********
+*	@ interfejs
+*		funkcja drukuj¹ca interfejs
+*	@ Argumenty:
+*		int &czas_wyk			adres zmiennej, która przechowuje wybor zakresow czasow wykonania
+		int &liczba_przest		adres zmiennej, która przechowuje wybor zakresu liczby przestojow
+		int &liczba_zadan		adres zmiennej, która przechowuje wybor zakresu liczby zadan
+*	@ Zwraca:
+*		
+*	 
+* 
+***********/
 void interfejs(int &czas_wyk,int &liczba_przest, int &liczba_zadan);
 
 
