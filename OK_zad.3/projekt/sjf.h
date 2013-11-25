83b085a0bb64e0698f5cdc3c5b74d4e80e44a332
mac229 <mac229@interia.eu>
2013-11-25 12:15:20 +0100

    DODANIE PLIKOW DO MOJEGO ALGORYTMU (SJF)
    sjf.h
    sjf.cpp

---
+++ OK_zad.3/projekt/sjf.h
@@ -0,0 +1,20 @@
+#ifndef SJF_H
+#define SJF_H
+
+#include<fstream>		//do plików
+#include<algorithm>		//do sortowania
+#include "generator.h"
+#include "maszyna.h"
+
+/***********
+*	TODO List
+*	
+*
+*		wyw³aszczanie
+*		zapis do pliku w odpowiednim formacie
+*
+***********/
+
+//void algorytmSJF(const Generator& generator);
+
+#endif //SJF_H
