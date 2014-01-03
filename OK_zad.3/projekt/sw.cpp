c81cae4a5f661dbab433e32730d39e70e912e758
chris710 <chris710@o2.pl>
2014-01-03 18:25:41 +0100

    PLAN

--- OK_zad.3/projekt/sw.cpp
+++ OK_zad.3/projekt/sw.cpp
@@ -190,6 +190,24 @@ void sortowanie(vector<Operacja*> & uszeregowanie, vector<Operacja*> & zadania) 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void wyzarzanie(const Generator& generator, int tablica[]){
+	/***********
+	*		TODO
+	*	-krok zmniejszania temperatury jako argument funkcji
+	*	-szukanie najgorszego przestoju
+	*	-kombinowanie z przestojami póki nie osi¹gniemy punktu tolerancji (uzale¿niæ go od temperatury)
+	*	-kombinowanie z zapychaczami
+	*		-szukanie losowego na start
+	*		-próba zamiany kolejnych s¹siadów tej operacji z lewej i z prawej
+	*		-sprawdzanie czy s¹siedzi znajduj¹ siê w granicach uszeregowania
+	*		-wsadzenie póŸniejszej operacji na wczeœniejsze miejsce
+	*		-przeliczenie wszysktich operacji nastêpuj¹cych po tej wczeœniejszej (ju¿ przestawionej), a¿ do odkrycia miejsca startowego operacji póŸniejszej
+	*		-sprawdzenie czy wczeœniejsza operacja mo¿e wejœæ na póŸniejsze miejsce
+	*		-je¿eli nie mo¿e to kontynuujemy pêtle zgodnie ze wzorem (n to numer zapychacza): n-1,n+1; n-1,n+2, n-2,n+1, n-2,n+2 itd...
+	*		-kontynuowanie pêtli póki nie osi¹gniemy oczekiwanej poprawy (podaæ jako argument?)
+	*	-kolejny wybór maszyny, a¿ do skoñczenia siê czasu
+	*	-zapisywanie najlepszego uszeregowania do wyniku
+	***********/
+
 
 	vector <Operacja*> zadania;																				// wektor do fukcji sortowanie 
 	Maszyna* maszyna;																						// wskaznik na dana maszyne
